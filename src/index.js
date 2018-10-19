dca3d3f3cb5cd3beb6e0a306816bddd44741731c
erickstm87 <dev@terickson.io>
2018-10-18 22:12:31 -0600

    trying without redux first

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers'; 
+import App from './App'; 
 import * as serviceWorker from './serviceWorker';
 
-
-const store = createStore(rootReducer)
-
 ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>, 
+    <App />,
     document.getElementById('root')
 );
 
--- src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import inputCalc from './inputCalc';
-
-export default combineReducers({
-    inputCalc
-})
